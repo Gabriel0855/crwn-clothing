72901f10e0bed5985cd0d5e5dfd5272aa457d485
Gabriel Thomas <gabrielthomas2017@u.northwestern.edu>
2020-05-12 15:41:44 -0300

    Added cart icon with dropdown functionality passed through redux in a cart reducer that hold cart state (hidden or not)

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './header.styles.scss';
 import {Link} from 'react-router-dom';
-import {ReactComponent as Logo} from '../../assets/original.svg'
+import {ReactComponent as Logo} from '../../assets/crown.svg';
 import {auth} from '../../firebase/firebase.utils';
 import {connect} from 'react-redux';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import toggleCartHidden from '../../redux/cart/cart.actions';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link to='/' className='logo-container'>
             <Logo className='logo' />
@@ -19,12 +22,19 @@ const Header = ({currentUser}) => (
                 :
                 <Link className='option' to='/signin'>SIGN-IN</Link>
             }
+            <Link className='option'><CartIcon /></Link>
         </div>
+        {
+            hidden ? null :
+            <CartDropdown /> 
+        }
+        
     </div>
 )
 
-const mapStateToProps = (state) => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart: {hidden}}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
